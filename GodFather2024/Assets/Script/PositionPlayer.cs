68166aed2e2aa3a74b6f1cc56eaf45dcf305173d
LucLenne <luc.lenne@outlook.fr>
2024-09-13 11:24:45 +0200

    advace gps

--- GodFather2024/Assets/Script/PositionPlayer.cs
+++ GodFather2024/Assets/Script/PositionPlayer.cs
@@ -8,6 +8,8 @@ public class PositionPlayer : MonoBehaviour
     [SerializeField]private List<TextMeshProUGUI> Names;
     [SerializeField] private List<TextMeshProUGUI> Pos;
     private string publicLeaderboardKey = "ba3c28f21071c93fc1038b73d3a39454e52d993894a3a76e42ca8e3161708156";
+    public int keyPlayer;
+
     private void Start()
     {
         GetLeaderBoard();
@@ -16,6 +18,7 @@ public class PositionPlayer : MonoBehaviour
     {
         LeaderboardCreator.GetLeaderboard(publicLeaderboardKey, ((msg) => {
             int loopLenght = (msg.Length < Names.Count) ? msg.Length : Names.Count;
+            keyPlayer = Names.Count-1;
             for (int i = 0; i < loopLenght; i++)
             {
                 Names[i].text = msg[i].Username;
@@ -23,6 +26,7 @@ public class PositionPlayer : MonoBehaviour
             }
         }));
     }
+
 
     public void SetLeaderboardEntry(string username, int pos)
     {
