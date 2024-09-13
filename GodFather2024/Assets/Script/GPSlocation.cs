aff23ec9c7087e347638aaa5d98dce3a3e26c0a0
LucLenne <luc.lenne@outlook.fr>
2024-09-13 09:35:22 +0200

    [EDIT]Scene

--- GodFather2024/Assets/Script/GPSlocation.cs
+++ GodFather2024/Assets/Script/GPSlocation.cs
@@ -9,6 +9,9 @@ public class GPSLocation : MonoBehaviour
     public Image testImg;
     public Text gpsOut;
     public int i;
+    public float GPSLatitude;
+    public float GPSLongitude;
+    public float GPSAltitude;
     void Start()
     {
         StartCoroutine(RunGPS());
@@ -46,6 +49,9 @@ public class GPSLocation : MonoBehaviour
         }
         else
         {
+            GPSAltitude = Input.location.lastData.altitude;
+            GPSLongitude = Input.location.lastData.longitude;
+            GPSLatitude = Input.location.lastData.latitude;
             // If the connection succeeded, this retrieves the device's current location and displays it in the Console window.
             gpsOut.text = "Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp;
         }
